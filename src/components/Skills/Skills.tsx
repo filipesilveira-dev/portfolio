ab3d97a51bdf0da4ea1b78fedbfc0c49afc7b8f9
Filipe <filipesilveira.dev@gmail.com>
2026-08-23 19:37:25 -0300

    feat: implementação do componente Skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,26 +1,33 @@
-import { Accordion } from "../accordeon/Accordion";
+import { Accordion } from "../accordion/Accordion";
+import styles from "./Skills.module.css";
+import { hardSkills, softSkills } from "../../utils/skills";
 
-export function Skills(){
-    return(
-        <Accordion title="Skills">
-            <div className="content">
-                <div className="skills">
-                    <section className="tools">
-                        <h3>Ferramentas:</h3>
-                        <ul id="profile.skills.hardSkills">
-                        </ul>
-                    </section>
+export function Skills() {
+  return (
+    <Accordion title="Skills">
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          <section className={styles.tools}>
+            <h3>Ferramentas:</h3>
+            <ul>
+              {hardSkills.map((skill) => (
+                <li>
+                  <img src={skill.logo} title={skill.name}></img>
+                </li>
+              ))}
+            </ul>
+          </section>
 
-                    <section className="personal">
-                        <h3>Pessoal:</h3>
-                        <ul id="profile.skills.softSkills">
-                            <li>Carregando...</li>
-                            <li>Carregando...</li>
-                            <li>Carregando...</li>
-                        </ul>
-                    </section>
-                </div>
-            </div>
-        </Accordion>
-    )
+          <section className={styles.personal}>
+            <h3>Pessoal:</h3>
+            <ul>
+              {softSkills.map((skill) => (
+                <li>{skill}</li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </div>
+    </Accordion>
+  );
 }
---
+++ src/utils/skills.ts
@@ -0,0 +1,42 @@
+export const hardSkills = [
+  {
+    name: "HTML5",
+    logo: "/src/assets/logos/html.png",
+  },
+  {
+    name: "CSS3",
+    logo: "/src/assets/logos/css.png",
+  },
+  {
+    name: "JavaScript",
+    logo: "/src/assets/logos/js.png",
+  },
+  {
+    name: "Nodejs",
+    logo: "/src/assets/logos/nodejs.png",
+  },
+  {
+    name: "React",
+    logo: "/src/assets/logos/react.png",
+  },
+  {
+    name: "Next.js",
+    logo: "/src/assets/logos/Next.js.png",
+  },
+  {
+    name: "Git",
+    logo: "/src/assets/logos/git.png",
+  },
+  {
+    name: "GitHub",
+    logo: "/src/assets/logos/github.png",
+  }
+];
+
+export const softSkills = [
+  "Proatividade",
+  "Resolutibilidade",
+  "Resiliência",
+  "Trabalho em equipe",
+  "Empatia",
+];
